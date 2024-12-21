da5ee99ddc0571b207994af6ad0d155506e992dc
Srikanth garimilla <srikanthg@technoflair.com>
2024-12-21 16:41:20 +0530

    ci:changes-for-docker -- Introduced docker compose

--- Email.Services/Email.Services.Processor/Program.cs
+++ Email.Services/Email.Services.Processor/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy(name:myAllowSpecificOrigins, policy =>
     {
-        policy.WithOrigins("http://localhost:4200")
+        policy.WithOrigins("http://localhost:8083")
               .AllowAnyHeader()
               .AllowAnyMethod()
               .AllowCredentials(); // Required for SignalR with credentials
